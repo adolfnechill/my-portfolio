02ffc5409c34a1d0008e9f04a4569708ca529fbf
yuwi <yuwebdev@mail.ru>
2026-03-15 18:10:41 +0300

    reworked formsvalidation

--- formvalidation/script.js
+++ formvalidation/script.js
@@ -93,7 +93,7 @@ class FormsValidation {
 
     if (!isFormValid) {
       event.preventDefault();
-      firstInvalidFieldControl.focus()
+      firstInvalidFieldControl.focus();
     }
   }
 
@@ -112,3 +112,61 @@ class FormsValidation {
 }
 
 new FormsValidation();
+
+class SwitchTheme {
+  selectors = {
+    switchThemeButton: "[data-js-theme-switcher]",
+  };
+
+  themes = {
+    light: "light",
+    dark: "dark",
+  };
+
+  stateClasses = {
+    isDarkTheme: "is-dark-theme",
+  };
+
+  storageKey = "theme";
+
+  constructor() {
+    this.switchThemeButtonElement = document.querySelector(
+      this.selectors.switchThemeButton,
+    );
+    this.setInitialTheme();
+    this.bindEvents();
+  }
+
+  getCachedTheme() {
+    return localStorage.getItem(this.storageKey);
+  }
+
+  setThemeToCache(theme) {
+    localStorage.setItem(this.storageKey, theme);
+  }
+
+  setInitialTheme() {
+    const isDarkThemeCached = this.getCachedTheme() === this.themes.dark;
+
+    document.documentElement.classList.toggle(
+      this.stateClasses.isDarkTheme,
+      isDarkThemeCached,
+    );
+  }
+
+  onClick = () => {
+    const isDarkThemeCached = this.getCachedTheme() === this.themes.dark;
+
+    this.setThemeToCache(
+      isDarkThemeCached ? this.themes.light : this.themes.dark,
+    );
+
+    document.documentElement.classList.toggle(this.stateClasses.isDarkTheme);
+  };
+
+  bindEvents() {
+    this.switchThemeButtonElement.addEventListener("click", this.onClick);
+  }
+}
+
+new SwitchTheme();
